22f63447e5b735d7cdf8d6a01d369729e1c0499d
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-02-20 17:06:11 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,24 @@
 
 const mobileHam = document.getElementById("mobile-ham");
 const mobileNav = document.getElementById("mobile-nav");
+
+let timer = null;
 mobileHam.addEventListener("click", (e) => {
     e.preventDefault();
-    mobileNav.classList.toggle("mobile-nav-toggle");
-})
+
+    if (timer) clearTimeout(timer);
+    if (mobileNav.classList.contains("mobile-nav-toggle")) {
+        mobileNav.classList.remove("mobile-nav-toggle");
+
+        timer = setTimeout(() => {
+            mobileNav.style.display = "none";
+        }, 250);
+    } 
+    else {
+        mobileNav.style.display = "block";
+
+        setTimeout(() => {
+            mobileNav.classList.add("mobile-nav-toggle");
+        }, 0);
+    };
+});
